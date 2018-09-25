d97f98e5f0e9685ec6110ed707c531873a9307ab
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-09-25 09:52:47 +0100

    basic express app

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello!');
+const express = require('express')
+const app = express();
+const port = 8000;
+
+app.get('/', (req,res) => {
+    res.send('i have node idea what im doing')
+})
+
+app.listen(port, () => console.log(`Up and running on localhost:${port}! 🎉`))
