cb63be90958fa472f8a85bce1b8944b1e54c81c9
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-09-25 10:06:49 +0100

    installed pug and set up 2 views

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express();
 const port = 8000;
 
+app.set('view engine', 'pug');
+
 app.get('/', (req,res) => {
-    res.send('i have node idea what im doing')
+    res.render('index')
+})
+
+app.get('/tell-me-more', (req, res) => {
+    res.render('tell-me-more')
 })
 
 app.listen(port, () => console.log(`Up and running on localhost:${port}! 🎉`))
